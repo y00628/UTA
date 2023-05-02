bfd713ac80656d1c90e3e8297ad6f20ea7fe9b25
BenjaminnHuang <peternick0207@gmail.com>
2023-05-02 01:19:35 -0700

    sign-in feature

--- source/scripts/home.js
+++ source/scripts/home.js
@@ -1,1 +1,32 @@
 
+const logInBtn = document.querySelector('.log-in');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close');
+const subBtn = document.querySelector('#submit-btn');
+const passcode = document.querySelector('#passcode');
+const form = document.querySelector('form');
+
+const password = '1234';
+
+logInBtn.addEventListener('click', ()=>{
+    modal.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', ()=>{
+    modal.style.display = 'none';
+    form.reset();
+})
+
+subBtn.addEventListener('click', ()=>{
+    if(passcode.value === password){
+        console.log("Pass!!");
+        form.reset();
+        window.location.href = "./source/protected.html";
+    }
+    else{
+        alert('Wrong Code!!')
+    }
+})
+
+
+
